ff00b9cebf3b90ccb8b82964990f4ab1fded2f25
yoga86 <yoga86@bk.ru>
2015-02-09 18:22:48 +0200

    1

---
+++ Panel1.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel1 {
+	public static void main(String[] args){
+		Panel1 gui = new Panel1();
+		gui.go();
+	}
+	public void go(){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.darkGray);
+		
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JButton button = new JButton("shock me");
+		JButton buttonTwo = new JButton("bliss");
+		JButton buttonThree = new JButton("huh?");
+		panel.add(button);
+		panel.add(buttonTwo);
+		panel.add(buttonThree);
+		
+		frame.getContentPane().add(BorderLayout.EAST, panel);
+		frame.setSize(250,200);
+		frame.setVisible(true);
+	}
+}
